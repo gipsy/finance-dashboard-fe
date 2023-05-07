62d61b2bfc8e1c54ebcdecce2480ed21703b8ba4
gipsy <ter4444@gmail.com>
2023-05-07 17:19:28 +0300

    adde redux-toolkit api boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,26 @@ import App                               from '@/App.tsx'
 import { DevSupport }                    from "@react-buddy/ide-toolbox";
 import { ComponentPreviews, useInitial } from "./dev";
 import '@/index.css';
+import { configureStore }                from "@reduxjs/toolkit";
+import { api }                           from "@/state/api";
+import { setupListeners }                from "@reduxjs/toolkit/query";
+import { Provider }                      from "react-redux";
+
+export const store = configureStore( {
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat( api.middleware ),
+} );
+
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot( document.getElementById( 'root' ) as HTMLElement ).render(
   <React.StrictMode>
     <DevSupport ComponentPreviews={ ComponentPreviews }
                 useInitialHook={ useInitial }
     >
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </DevSupport>
   </React.StrictMode>,
 )
