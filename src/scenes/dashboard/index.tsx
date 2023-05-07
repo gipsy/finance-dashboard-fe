0b6bc42e8784c5512904ea91599089d1deda477e
gipsy <ter4444@gmail.com>
2023-05-07 16:56:39 +0300

    refactored 3 Rows on Dashboard into separated components

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
-import DashboardBox                     from "@/components/DashboardBox";
-
-type Props = {};
+import Row1                   from "@/scenes/dashboard/Row1";
+import Row2                   from "@/scenes/dashboard/Row2";
+import Row3                   from "@/scenes/dashboard/Row3";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -49,7 +49,7 @@ const gridTemplateSmallScreens = `
   "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   
   return (
@@ -64,16 +64,9 @@ const Dashboard = (props: Props) => {
            gridTemplateAreas: gridTemplateSmallScreens,
          }}
     >
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
