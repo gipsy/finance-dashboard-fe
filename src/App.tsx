83823d75b712945af2adcae82071e675d6d224f6
gipsy <ter4444@gmail.com>
2023-05-07 16:49:07 +0300

    created Dashboard and its grid template layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { themeSettings }                                from "@/theme";
 import { BrowserRouter, Routes, Route }                 from "react-router-dom";
 import Navbar                                           from "@/scenes/navbar";
+import Dashboard                                        from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo( () => createTheme( themeSettings ), [] );
@@ -15,7 +16,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={ <div>Dashboard</div> }/>
+              <Route path="/" element={ <Dashboard /> }/>
               <Route path="/predictions" element={ <div>Predictions</div> }/>
             </Routes>
           </Box>
