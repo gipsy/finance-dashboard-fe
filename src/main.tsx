3a05a90397ecf7d58a015db88d9374c51d93921e
gipsy <ter4444@gmail.com>
2023-05-07 15:28:31 +0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React                             from 'react'
+import ReactDOM                          from 'react-dom/client'
+import App                               from '@/App.tsx'
+import { DevSupport }                    from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
+import '@/index.css';
+
+ReactDOM.createRoot( document.getElementById( 'root' ) as HTMLElement ).render(
+  <React.StrictMode>
+    <DevSupport ComponentPreviews={ ComponentPreviews }
+                useInitialHook={ useInitial }
+    >
+      <App/>
+    </DevSupport>
+  </React.StrictMode>,
+)
