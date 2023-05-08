a8247647d8707ff43fde7048ba63dca5ee716651
gipsy <ter4444@gmail.com>
2023-05-08 13:53:43 +0300

    added getKpisQuery reducer/api endpoint, added recharts logic for Row1 component

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
-import Row1                   from "@/scenes/dashboard/Row1";
-import Row2                   from "@/scenes/dashboard/Row2";
-import Row3                   from "@/scenes/dashboard/Row3";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -15,7 +15,6 @@ const gridTemplateLargeScreens = `
   "g h j"
   "g h j"
 `;
-
 const gridTemplateSmallScreens = `
   "a"
   "a"
@@ -51,18 +50,25 @@ const gridTemplateSmallScreens = `
 
 const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
-  
   return (
-    <Box width="100%" height="100%" display="grid" gap="1.5rem"
-         sx={isAboveMediumScreens ? {
-           gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-           gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-           gridTemplateAreas: gridTemplateLargeScreens,
-         }: {
-           gridAutoColumns: "1fr",
-           gridAutoRows: "80px",
-           gridTemplateAreas: gridTemplateSmallScreens,
-         }}
+    <Box
+      width="100%"
+      maxHeight={ 300 }
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+        ? {
+            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+            gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+            gridTemplateAreas: gridTemplateLargeScreens,
+          }
+        : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens,
+          }
+      }
     >
       <Row1 />
       <Row2 />
@@ -70,5 +76,5 @@ const Dashboard = () => {
     </Box>
   );
 };
-export default Dashboard;
 
+export default Dashboard;
